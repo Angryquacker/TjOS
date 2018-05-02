133a87dea185b81ab245c70dffc6549c16fbff0b
Travis <tjxhahn@gmail.com>
2018-05-02 10:57:12 -1200

    Add files via upload

--- TjOS/JS/Desktop.js
+++ TjOS/JS/Desktop.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
     //Hide Menus
     $("#home").hide();
     $("#console").hide();
+    $("#area").hide();
     let homeOn = false;
     let deskOn = true;
     let consoleOn = false;
+    let areaOn = false;
+
+    //Set Text to Last Saved
+    document.getElementById("box").value = document.cookie;
 
     //Show || Hide Menu Logic
     $("#homeOn").click(function() {
@@ -38,17 +43,47 @@ $(document).ready(function() {
             consoleOn = false;
         }
     });
+
+//Show || Hide Text Box Logic
+    $("#text").click(function() {
+        if (areaOn == false) {
+            $("#area").show();
+            areaOn = true;
+        } else {
+            $("#area").hide();
+            areaOn = false;
+        }
+    });
 });
 
+//Console Commands
 function executeCommand() {
+    let help = "<p>echo - Outputs Text<br />help - Shows Comamnds<br />clear - Clears All Text</p>";
     let command = document.getElementById("com").value;
     let params = command.split(" ");
-    switch(params[0]) {
+    let com = params.shift();
+    let state = params.join(" ");
+    switch(com.toLowerCase()) {
         case "echo":
-            $("#result").text(params[1]);
+            $("#result").text(state);
+            break;
+        case "help":
+            $("#result").html(help);
+            break;
+        case "clear":
+            $("#result").text("");
+            document.getElementById("com").value = "";
             break;
         default:
             $("#result").text("Unknown Command");
             break;
     }
 }
+
+//Test Function For Saving Text as a Cookie
+/* function saveText() {
+    document.cookie = "text=" + document.getElementById('box').value;
+    let x = document.cookie;
+    alert(x);
+    alert(document.getElementById('box').value);
+} */
