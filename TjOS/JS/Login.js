30a4c718d35cbf9978d67ef69d7bb874df518658
Travis <tjxhahn@gmail.com>
2018-05-08 21:48:09 -0400

    Update Login.js

--- TjOS/JS/Login.js
+++ TjOS/JS/Login.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     $("#wel").hide();
     $("#wel").show(1500);
+    
+    $("button").click(function() {
+        if(document.getElementById('wel').value != undefined) {
+            alert('Welcome ' + document.getElementById('wel').value);
+        } else {
+            alert('Welcome');
+        }
+    });
 });
