5ca174910764e97aadd6ba2a668a7abc11f2e434
Travis <tjxhahn@gmail.com>
2018-05-17 22:49:58 -0400

    Update Desktop.js

--- TjOS/JS/Desktop.js
+++ TjOS/JS/Desktop.js
@@ -65,7 +65,7 @@ $(document).ready(function() {
 
 //Console Commands
 function executeCommand() {
-    let help = "<p>echo - Outputs Text<br />help - Shows Comamnds<br />clear - Clears All Text</p>";
+    let help = "<p>echo - Outputs Text<br />help - Shows Comamnds<br />clear - Clears All Text<br />background - Sets the Background Color</p>";
     let command = document.getElementById("com").value;
     let params = command.split(" ");
     let com = params.shift();
